c8d3860f021482e31a3e58f4304dd73e73bb5d9b
Nail Gafarov <nil20122013@gmail.com>
2022-11-16 17:47:28 +0500

    Implemented parse() function

--- src/semver.ts
+++ src/semver.ts
@@ -5,21 +5,48 @@ const REGEX_SEMVER = /^(?<major>\d+)\.(?<minor>\d+)\.(?<patch>\d+)(?:-(?<!\.)(?<
 export function compare(v1: string, v2: string): -1 | 0 | 1 {}
 
 // TODO
-export function next(v: string, power: "major" | "minor" | "patch" | "pre-release" | "build"): string {}
+export function next(v: string, power: "major" | "minor" | "patch" | "prerelease" | "build"): string {}
 
-// TODO
-export function valid(v: string): boolean {}
-
-// TODO
-export function parse(v: string): Version {}
+/**
+ * Parses the given semver string into object.
+ * @param v String to parse.
+ * @returns Parsed object or `null` if the string is not a correct semver.
+ * @example
+ * ```ts
+ * const data = parse("1.2.3-45+67");
+ * {
+ * 	major: 1,
+ * 	minor: 2,
+ * 	patch: 3,
+ * 	prerelease: "45",
+ * 	build: "67"
+ * }
+ * ```
+ */
+export function parse(v: string): Version | null {
+	const match = v.match(REGEX_SEMVER);
+	if (!match || !match.groups)
+		return null;
+	const result = match.groups as unknown as Version;
+	if (result.prerelease && !isMetadataValid(result.prerelease) || result.build && !isMetadataValid(result.build))
+		return null;
+	result.major = +result.major;
+	result.minor = +result.minor;
+	result.patch = +result.patch;
+	return result;
+}
 
 // TODO
 export function stringify(v: Version): string {}
+
+function isMetadataValid(data: string): boolean {
+	return data.split(".").every(s => s);
+}
 
 type Version = {
 	major: number;
 	minor: number;
 	patch: number;
-	preRelease?: string;
+	prerelease?: string;
 	build?: string;
 }
