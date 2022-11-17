eeacf6302013f458233c0b48aee991a3c46d1664
Nail Gafarov <nil20122013@gmail.com>
2022-11-17 09:50:13 +0500

    Implemented format() function

--- src/string.ts
+++ src/string.ts
@@ -33,5 +33,55 @@ export function unescape(s: string, char: string = "\""): string {
 	return result;
 }
 
-// TODO
-export function format(s: string, ...args: any[]): string {}
+/**
+ * Formats the given string using passed arguments. Format string uses numeric zero-indexed placeholders enclosed in
+ * curly braces. The placeholder braces can be escaped using backslash.
+ * @param s Format string.
+ * @param args Arguments to replace placeholders in the string.
+ * @returns Formatted string.
+ * @example
+ * ```ts
+ * format("Hello, {0}", "user"); // "Hello, user"
+ * ```
+ */
+export function format(s: string, ...args: any[]): string {
+	let result = "";
+	let isEscape = false;
+	let placeholder: string | null = null;
+	for (const c of s) {
+		if (c === "\\") {
+			isEscape = true;
+			continue;
+		}
+		switch (c) {
+			case "{": {
+				if (isEscape) {
+					result += c;
+				} else {
+					placeholder = "";
+				}
+				break;
+			}
+			case "}": {
+				if (isEscape || placeholder == null) {
+					result += c;
+				} else {
+					result += String(args[+placeholder]);
+					placeholder = null;
+				}
+				break;
+			}
+			default: {
+				if (placeholder == null) {
+					result += c;
+				} else {
+					placeholder += c;
+				}
+			}
+		}
+		isEscape = false;
+	}
+	if (placeholder != null)
+		result += placeholder || "{";
+	return result;
+}
