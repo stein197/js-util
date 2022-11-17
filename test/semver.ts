1acd8e67dd9a37581f9edd5fb48e29b9d176e51b
Nail Gafarov <nil20122013@gmail.com>
2022-11-17 13:03:16 +0500

    Added semver test stubs

---
+++ test/semver.ts
@@ -0,0 +1,15 @@
+import * as assert from "assert";
+import * as mocha from "mocha";
+import * as semver from "../src/semver";
+
+// TODO
+mocha.describe("compare()", () => {});
+
+// TODO
+mocha.describe("next()", () => {});
+
+// TODO
+mocha.describe("parse()", () => {});
+
+// TODO
+mocha.describe("stringify()", () => {});
