d2cc265edb789f9ab6ea4f011ed75ab7dd0974e7
Nail Gafarov <nil20122013@gmail.com>
2022-11-17 13:34:30 +0500

    Completed tests for semver.compare() function

--- test/semver.ts
+++ test/semver.ts
@@ -4,38 +4,86 @@ import * as semver from "../src/semver";
 
 mocha.describe("compare()", () => {
 	mocha.describe("Comparing main numbers without metadata", () => {
-		mocha.it.skip("Should return -1 major number is lesser in the first version", () => {});
-		mocha.it.skip("Should return 1 major number is greater in the first version", () => {});
-		mocha.it.skip("Should return -1 minor number is lesser in the first version", () => {});
-		mocha.it.skip("Should return 1 minor number is greater in the first version", () => {});
-		mocha.it.skip("Should return -1 patch number is lesser in the first version", () => {});
-		mocha.it.skip("Should return 1 patch number is greater in the first version", () => {});
+		mocha.it("Should return -1 when the major number is lesser in the first version", () => {
+			assert.equal(semver.compare("1.9.9", "2.0.0"), -1);
+		});
+		mocha.it("Should return 1 when the major number is greater in the first version", () => {
+			assert.equal(semver.compare("2.0.0", "1.9.9"), 1);
+		});
+		mocha.it("Should return -1 when the minor number is lesser in the first version", () => {
+			assert.equal(semver.compare("1.1.9", "1.2.0"), -1);
+		});
+		mocha.it("Should return 1 when the minor number is greater in the first version", () => {
+			assert.equal(semver.compare("1.2.0", "1.1.9"), 1);
+		});
+		mocha.it("Should return -1 when the patch number is lesser in the first version", () => {
+			assert.equal(semver.compare("1.1.1", "1.1.2"), -1);
+		});
+		mocha.it("Should return 1 when the patch number is greater in the first version", () => {
+			assert.equal(semver.compare("1.1.2", "1.1.1", ), 1);
+		});
 	});
 	mocha.describe("Comparing main numbers with metadata", () => {
-		mocha.it.skip("Should return -1 when major number is lesser in the first version", () => {});
-		mocha.it.skip("Should return 1 when major number is greater in the first version", () => {});
-		mocha.it.skip("Should return -1 when minor number is lesser in the first version", () => {});
-		mocha.it.skip("Should return 1 when minor number is greater in the first version", () => {});
-		mocha.it.skip("Should return -1 when patch number is lesser in the first version", () => {});
-		mocha.it.skip("Should return 1 when patch number is greater in the first version", () => {});
+		mocha.it("Should return -1 when the major number is lesser in the first version", () => {
+			assert.equal(semver.compare("1.9.9-100+100", "2.0.0-10+10"), -1);
+		});
+		mocha.it("Should return 1 when the major number is greater in the first version", () => {
+			assert.equal(semver.compare("2.0.0-10+10", "1.9.9-100+100"), 1);
+		});
+		mocha.it("Should return -1 when the minor number is lesser in the first version", () => {
+			assert.equal(semver.compare("1.1.9-100+100", "1.2.0-10+10"), -1);
+		});
+		mocha.it("Should return 1 when the minor number is greater in the first version", () => {
+			assert.equal(semver.compare("1.2.0-10+10", "1.1.9-100+100"), 1);
+		});
+		mocha.it("Should return -1 when the patch number is lesser in the first version", () => {
+			assert.equal(semver.compare("1.1.1-100+100", "1.1.2-10+10"), -1);
+		});
+		mocha.it("Should return 1 when the patch number is greater in the first version", () => {
+			assert.equal(semver.compare("1.1.2-10+10", "1.1.1-100+100", ), 1);
+		});
 	});
 	mocha.describe("Main numbers are equal but pre-release parts differ", () => {
-		mocha.it.skip("Should return -1 when the first version has pre-release metadata but the second one does not", () => {});
-		mocha.it.skip("Should return 1 when the first version does not have pre-release metadata but the second one does", () => {});
-		mocha.it.skip("Should return -1 when the first pre-release metadata has more dot-separated parts than the second one", () => {});
-		mocha.it.skip("Should return 1 when the first pre-release metadata has less dot-separated parts than the second one", () => {});
-		mocha.it.skip("Should return -1 when both pre-release metadata has the same amount of parts but the first pre-release is lexicographically lesser", () => {});
-		mocha.it.skip("Should return 1 when both pre-release metadata has the same amount of parts but the first pre-release is lexicographically greater", () => {});
+		mocha.it("Should return -1 when the first version has pre-release metadata but the second one does not", () => {
+			assert.equal(semver.compare("0.0.0-1", "0.0.0", ), -1);
+		});
+		mocha.it("Should return 1 when the first version does not have pre-release metadata but the second one does", () => {
+			assert.equal(semver.compare("0.0.0", "0.0.0-1"), 1);
+		});
+		mocha.it("Should return -1 when the first pre-release metadata has less dot-separated parts than the second one", () => {
+			assert.equal(semver.compare("0.0.0-0", "0.0.0-0.0"), -1);
+		});
+		mocha.it("Should return 1 when the first pre-release metadata has more dot-separated parts than the second one", () => {
+			assert.equal(semver.compare("0.0.0-0.0", "0.0.0-0"), 1);
+		});
+		mocha.it("Should return -1 when both pre-release metadata has the same amount of parts but the first pre-release is lexicographically lesser", () => {
+			assert.equal(semver.compare("0.0.0-a.1.0", "0.0.0-a.1.1"), -1);
+		});
+		mocha.it("Should return 1 when both pre-release metadata has the same amount of parts but the first pre-release is lexicographically greater", () => {
+			assert.equal(semver.compare("0.0.0-a.1.1", "0.0.0-a.1.0"), 1);
+		});
 	});
 	mocha.describe("Equal versions", () => {
-		mocha.it.skip("Should return 0 when all main numbers are equal and there is no any metadata", () => {});
-		mocha.it.skip("Should return 0 when all main numbers and prerelease are equal and there is no build metadata", () => {});
-		mocha.it.skip("Should return 0 when all main numbers and prerelease are equal and the build metadata differs", () => {});
-		mocha.it.skip("Should return 0 when the strings are identical", () => {});
+		mocha.it("Should return 0 when all main numbers are equal and there is no any metadata", () => {
+			assert.equal(semver.compare("1.2.3", "1.2.3"), 0);
+		});
+		mocha.it("Should return 0 when all main numbers and prerelease are equal and there is no build metadata", () => {
+			assert.equal(semver.compare("1.2.3-4", "1.2.3-4"), 0);
+		});
+		mocha.it("Should return 0 when all main numbers and prerelease are equal and the build metadata differs", () => {
+			assert.equal(semver.compare("1.2.3-4+123", "1.2.3-4+abc"), 0);
+		});
+		mocha.it("Should return 0 when the strings are identical", () => {
+			assert.equal(semver.compare("1.2.3-45+67", "1.2.3-45+67"), 0);
+		});
 	});
 	mocha.describe("Parse errors", () => {
-		mocha.it.skip("Should throw an error when the first version is invalid", () => {});
-		mocha.it.skip("Should throw an error when the second version is invalid", () => {});
+		mocha.it("Should throw an error when the first version is invalid", () => {
+			assert.throws(() => semver.compare("invalid", "1.2.3"), {message: "\"invalid\" is not a valid semver string"});
+		});
+		mocha.it("Should throw an error when the second version is invalid", () => {
+			assert.throws(() => semver.compare("1.2.3", "invalid"), {message: "\"invalid\" is not a valid semver string"});
+		});
 	});
 });
 
