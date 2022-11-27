a6d4ab1c2c3879119e6e92f7cf235773ee4f2ff8
Nail Gafarov <nil20122013@gmail.com>
2022-11-27 13:11:39 +0500

    Test stubs for byte

---
+++ test/byte.ts
@@ -0,0 +1,9 @@
+import * as assert from "assert";
+import * as mocha from "mocha";
+import * as byte from "../src/byte";
+
+// TODO
+mocha.describe("parse()", () => {});
+
+// TODO
+mocha.describe("format()", () => {});
