e19b683685f428b56ba2aa661d06c205864411f2
Nail Gafarov <nil20122013@gmail.com>
2022-11-28 17:02:44 +0500

    Setup for roman module

---
+++ src/roman.ts
@@ -0,0 +1,16 @@
+const MAX_CHARS = 3;
+const DICTIONARY = {
+	I: 1,
+	V: 5,
+	X: 10,
+	L: 50,
+	C: 100,
+	D: 500,
+	M: 1000
+};
+
+// TODO
+export function parse(num: string): number {}
+
+// TODO
+export function stringify(num: number): string {}
---
+++ test/roman.ts
@@ -0,0 +1,8 @@
+import * as assert from "assert";
+import * as mocha from "mocha";
+import * as roman from "../src/roman";
+
+// TODO
+mocha.describe("parse()", () => {});
+// TODO
+mocha.describe("stringify()", () => {});
