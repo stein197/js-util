a2bc2fa69e9fa27c920d9f97fc2879ce86d90245
Nail Gafarov <nil20122013@gmail.com>
2022-12-09 13:50:44 +0500

    Added equal() and clone() functions

--- src/object.ts
+++ src/object.ts
@@ -14,6 +14,78 @@ export function deepFreeze(object: any): void {
 	applyRecursive(Object.freeze, object);
 }
 
+/**
+ * Same as {@link Object.preventExtensions} but performs the prevention recursively.
+ * @param object Object to prevent extensions of.
+ */
+export function deepPreventExtensions(object: any): void {
+	applyRecursive(Object.preventExtensions, object);
+}
+
+/**
+ * Checks if two objects are deeply equal. It makes partial comparison.
+ * @param a First object to compare.
+ * @param b Second object to compare.
+ * @returns `true` if both objects are strictly deeply equal, `-1` if the first object is a subset of the second one,
+ *          `1` if the second one is a subset of the first one and `false` otherwise.
+ */
+export function equal(a: any, b: any): -1 | 1 | boolean {
+	if (a === b)
+		return true;
+	if (a != null && b != null && typeof a === "object" && typeof b === "object") {
+		const aEntries = Object.entries(a);
+		const bEntries = Object.entries(b);
+		let minorEntries: [string, any][];
+		let majorObject: any;
+		if (aEntries.length < bEntries.length) {
+			minorEntries = aEntries;
+			majorObject = b;
+		} else {
+			minorEntries = bEntries;
+			majorObject = a;
+		}
+		const returnValue = aEntries.length === bEntries.length ? true : (aEntries.length < bEntries.length ? -1 : 1);
+		const result = minorEntries.map(entry => entry[0] in majorObject ? equal(majorObject[entry[0] as any], entry[1]) : false).reduce((acc, v) => acc === true || acc === v ? v : false, true);
+		return aEntries.length === bEntries.length ? result : (result ? returnValue : false);
+	}
+	return false;
+}
+
+/**
+ * Checks if two objects are deeply and partly equal.
+ * @param base Base object to compare with.
+ * @param part Partial object of the first one.
+ * @returns `true` if the second object is a subset of the second one.
+ */
+export function partlyEqual(base: any, part: any): boolean {
+	const result = equal(base, part);
+	return +result > 0;
+}
+
+/**
+ * Checks if two objects are deeply and strictly equal.
+ * @param a First object to compare.
+ * @param b Second object to compare.
+ * @returns `true` if both objects are strictly deeply equal.
+ */
+export function strictlyEqual(a: any, b: any): boolean {
+	return equal(a, b) === true;
+}
+
+/**
+ * Perform deep object clone.
+ * @param arg Object to clone.
+ * @returns Cloned object.
+ */
+export function clone<T>(arg: T): T {
+	if (typeof arg !== "object" || arg == null)
+		return arg;
+	const result: any = Array.isArray(arg) ? new Array(arg.length) : {}
+	for (const [key, value] of Object.entries(arg!))
+		result[key] = clone(value);
+	return result;
+}
+
 function applyRecursive(f: (o: any) => void, object: any): void {
 	if (object == null || typeof object !== "object")
 		return;
--- test/object.ts
+++ test/object.ts
@@ -2,6 +2,15 @@ import * as assert from "assert";
 import * as mocha from "mocha";
 import * as object from "../src/object";
 
+const complexArray = [1, [3, {e: 5, f: 6}]];
+const complexArrayCopy = [1, [3, {e: 5, f: 6}]];
+const diffComplexArray = [1, [3, [5]]];
+const partComplexArray = [1, [3, {}]];
+const complexObject = {a: 1, b: {c: [1, 2, 3]}};
+const complexObjectCopy = {a: 1, b: {c: [1, 2, 3]}};
+const diffComplexObject = {a: 1, b: {c: [1, 3]}};
+const partComplexObject = {b: {c: []}};
+
 mocha.describe("deepSeal()", () => {
 	mocha.it("Should recursively seal an object", () => {
 		const o = stub();
@@ -19,6 +28,143 @@ mocha.describe("deepFreeze()", () => {
 		assert.equal(Object.isFrozen(o), true);
 		assert.equal(Object.isFrozen(o.b), true);
 		assert.equal(Object.isFrozen(o.b.d), true);
+	});
+});
+
+mocha.describe("deepPreventExtensions()", () => {
+	mocha.it("Should recursively prevent extensions", () => {
+		const o = stub();
+		object.deepPreventExtensions(o);
+		assert.equal(Object.isExtensible(o), false);
+		assert.equal(Object.isExtensible(o.b), false);
+		assert.equal(Object.isExtensible(o.b.d), false);
+	});
+});
+
+mocha.describe("equal()", () => {
+
+	mocha.describe("Nulls", () => {
+		mocha.it("equal(null, null) == true", () => assert.equal(object.equal(null, null), true));
+	});
+
+	mocha.describe("Booleans", () => {
+		mocha.it("Equal booleans == true", () => assert.equal(object.equal(true, true), true));
+		mocha.it("Unequal booleans == false", () => assert.equal(object.equal(true, false), false));
+	});
+
+	mocha.describe("Numbers", () => {
+		mocha.it("Equal numbers == true", () => assert.equal(object.equal(1, 1), true));
+		mocha.it("Unequal numbers == false", () => assert.equal(object.equal(1, 2), false));
+	});
+
+	mocha.describe("Strings", () => {
+		mocha.it("Equal strings == true", () => assert.equal(object.equal("a", "a"), true));
+		mocha.it("Empty strings == true", () => assert.equal(object.equal("", ""), true));
+		mocha.it("Unequal strings == false", () => assert.equal(object.equal("a", "b"), false));
+		mocha.it("String with empty one == false", () => assert.equal(object.equal("a", ""), false));
+	});
+
+	mocha.describe("Arrays", () => {
+		mocha.it("Empty arrays == true", () => assert.equal(object.equal([], []), true));
+		mocha.it("Equal arrays == true", () => assert.equal(object.equal(["a"], ["a"]), true));
+		mocha.it("Part with full array == -1", () => assert.equal(object.equal(["a"], ["a", "b"]), -1));
+		mocha.it("Full with part array == 1", () => assert.equal(object.equal(["a", "b"], ["a"]), 1));
+		mocha.it("Unequal arrays == false", () => assert.equal(object.equal(["a", "b"], ["b", "a"]), false));
+		mocha.it("Equal complex arrays == true", () => assert.equal(object.equal(complexArray, complexArrayCopy), true));
+		mocha.it("Unequal complex arrays == false", () => assert.equal(object.equal(complexArray, diffComplexArray), false));
+		mocha.it("Part with full complex array == -1", () => assert.equal(object.equal(partComplexArray, complexArray), -1));
+		mocha.it("Full with part complex array == 1", () => assert.equal(object.equal(complexArray, partComplexArray), 1));
+	});
+
+	mocha.describe("Objects", () => {
+		mocha.it("Empty objects == true", () => assert.equal(object.equal({}, {}), true));
+		mocha.it("Equal objects == true", () => assert.equal(object.equal({a: 1}, {a: 1}), true));
+		mocha.it("Part with full object == -1", () => assert.equal(object.equal({a: 1}, {a: 1, b: 2}), -1));
+		mocha.it("Full with part object == 1", () => assert.equal(object.equal({a: 1, b: 2}, {a: 1}), 1));
+		mocha.it("Unequal objects == false", () => assert.equal(object.equal({a: 1}, {b: 2}), false));
+		mocha.it("Equal complex objects == true", () => assert.equal(object.equal(complexObject, complexObjectCopy), true));
+		mocha.it("Unequal complex objects == false", () => assert.equal(object.equal(complexObject, diffComplexObject), false));
+		mocha.it("Part with full complex object == -1", () => assert.equal(object.equal(partComplexObject, complexObject), -1));
+		mocha.it("Full with part complex object == 1", () => assert.equal(object.equal(complexObject, partComplexObject), 1));
+	});
+
+	mocha.describe("Mixed types with the same values", () => {
+		mocha.it("equal(1, \"1\") == false", () => assert.equal(object.equal(1, "1"), false));
+		mocha.it("equal(\"\", null) == false", () => assert.equal(object.equal("", null), false));
+		mocha.it("equal([1], [\"1\"]) == false", () => assert.equal(object.equal([1], ["1"]), false));
+		mocha.it("equal([], null) == false", () => assert.equal(object.equal([], null), false));
+		mocha.it("equal({}, null) == false", () => assert.equal(object.equal({}, null), false));
+	});
+});
+
+mocha.describe("partlyEqual()", () => {
+	mocha.describe("Arrays", () => {
+		mocha.it("Empty arrays == true", () => assert.equal(object.partlyEqual([], []), true));
+		mocha.it("Equal arrays == true", () => assert.equal(object.partlyEqual(["a"], ["a"]), true));
+		mocha.it("Part with full array == false", () => assert.equal(object.partlyEqual(["a"], ["a", "b"]), false));
+		mocha.it("Full with part array == true", () => assert.equal(object.partlyEqual(["a", "b"], ["a"]), true));
+		mocha.it("Unequal arrays == false", () => assert.equal(object.partlyEqual(["a", "b"], ["b", "a"]), false));
+		mocha.it("Equal complex arrays == true", () => assert.equal(object.partlyEqual(complexArray, complexArrayCopy), true));
+		mocha.it("Unequal complex arrays == false", () => assert.equal(object.partlyEqual(complexArray, diffComplexArray), false));
+		mocha.it("Part with full complex array == false", () => assert.equal(object.partlyEqual(partComplexArray, complexArray), false));
+		mocha.it("Full with part complex array == true", () => assert.equal(object.partlyEqual(complexArray, partComplexArray), true));
+	});
+
+	mocha.describe("Objects", () => {
+		mocha.it("Empty objects == true", () => assert.equal(object.partlyEqual({}, {}), true));
+		mocha.it("Equal objects == true", () => assert.equal(object.partlyEqual({a: 1}, {a: 1}), true));
+		mocha.it("Part with full object == false", () => assert.equal(object.partlyEqual({a: 1}, {a: 1, b: 2}), false));
+		mocha.it("Full with part object == true", () => assert.equal(object.partlyEqual({a: 1, b: 2}, {a: 1}), true));
+		mocha.it("Unequal objects == false", () => assert.equal(object.partlyEqual({a: 1}, {b: 2}), false));
+		mocha.it("Equal complex objects == true", () => assert.equal(object.partlyEqual(complexObject, complexObjectCopy), true));
+		mocha.it("Unequal complex objects == false", () => assert.equal(object.partlyEqual(complexObject, diffComplexObject), false));
+		mocha.it("Part with full complex object == false", () => assert.equal(object.partlyEqual(partComplexObject, complexObject), false));
+		mocha.it("Full with part complex object == true", () => assert.equal(object.partlyEqual(complexObject, partComplexObject), true));
+	});
+});
+
+mocha.describe("strictlyEqual()", () => {
+	mocha.describe("Arrays", () => {
+		mocha.it("Empty arrays == true", () => assert.equal(object.strictlyEqual([], []), true));
+		mocha.it("Equal arrays == true", () => assert.equal(object.strictlyEqual(["a"], ["a"]), true));
+		mocha.it("Part with full array == false", () => assert.equal(object.strictlyEqual(["a"], ["a", "b"]), false));
+		mocha.it("Full with part array == false", () => assert.equal(object.strictlyEqual(["a", "b"], ["a"]), false));
+		mocha.it("Unequal arrays == false", () => assert.equal(object.strictlyEqual(["a"], ["b"]), false));
+		mocha.it("Equal complex arrays == true", () => assert.equal(object.strictlyEqual(complexArray, complexArrayCopy), true));
+		mocha.it("Unequal complex arrays == false", () => assert.equal(object.strictlyEqual(complexArray, diffComplexArray), false));
+		mocha.it("Part with full complex array == false", () => assert.equal(object.strictlyEqual(partComplexArray, complexArray), false));
+		mocha.it("Full with part complex array == false", () => assert.equal(object.strictlyEqual(complexArray, partComplexArray), false));
+	});
+
+	mocha.describe("Objects", () => {
+		mocha.it("Empty objects == true", () => assert.equal(object.strictlyEqual({}, {}), true));
+		mocha.it("Equal objects == true", () => assert.equal(object.strictlyEqual({a: 1}, {a: 1}), true));
+		mocha.it("Part with full object == false", () => assert.equal(object.strictlyEqual({a: 1}, {a: 1, b: 2}), false));
+		mocha.it("Full with part object == false", () => assert.equal(object.strictlyEqual({a: 1, b: 2}, {a: 1}), false));
+		mocha.it("Unequal objects == false", () => assert.equal(object.strictlyEqual({a: 1}, {b: 2}), false));
+		mocha.it("Equal complex objects == true", () => assert.equal(object.strictlyEqual(complexObject, complexObjectCopy), true));
+		mocha.it("Unequal complex objects == false", () => assert.equal(object.strictlyEqual(complexObject, diffComplexObject), false));
+		mocha.it("Part with full complex object == false", () => assert.equal(object.strictlyEqual(partComplexObject, complexObject), false));
+		mocha.it("Full with part complex object == false", () => assert.equal(object.strictlyEqual(complexObject, partComplexObject), false));
+	});
+});
+
+mocha.describe("clone()", () => {
+	mocha.describe("Primitives", () => {
+		mocha.it("clone(null) === null", () => assert.equal(object.clone(null), null));
+		mocha.it("clone(true) === true", () => assert.equal(object.clone(true), true));
+		mocha.it("clone(1) === 1", () => assert.equal(object.clone(1), 1));
+		mocha.it("clone(\"a\") === \"a\"", () => assert.equal(object.clone("a"), "a"));
+	});
+
+	mocha.describe("Arrays", () => {
+		mocha.it("Returns equal object", () => assert.deepStrictEqual(object.clone(complexArray), complexArrayCopy));
+		mocha.it("Returns different reference", () => assert.equal(object.clone(complexArray) !== complexArray, true));
+	});
+
+	mocha.describe("Objects", () => {
+		mocha.it("Returns equal object", () => assert.deepStrictEqual(object.clone(complexObject), complexObjectCopy));
+		mocha.it("Returns different reference", () => assert.equal(object.clone(complexObject) !== complexObject, true));
 	});
 });
 
