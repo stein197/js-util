68ef6dfa35ceb1523918a9acc2d23b3a06e9a155
Nail Gafarov <nil20122013@gmail.com>
2022-12-09 09:40:56 +0500

    Implemented tests for object module

--- test/object.ts
+++ test/object.ts
@@ -2,8 +2,34 @@ import * as assert from "assert";
 import * as mocha from "mocha";
 import * as object from "../src/object";
 
-// TODO
-mocha.describe("deepSeal()", () => {});
+mocha.describe("deepSeal()", () => {
+	mocha.it("Should recursively seal an object", () => {
+		const o = stub();
+		object.deepSeal(o);
+		assert.equal(Object.isSealed(o), true);
+		assert.equal(Object.isSealed(o.b), true);
+		assert.equal(Object.isSealed(o.b.d), true);
+	});
+});
 
-// TODO
-mocha.describe("deepFreeze()", () => {});
+mocha.describe("deepFreeze()", () => {
+	mocha.it("Should recursively freeze an object", () => {
+		const o = stub();
+		object.deepFreeze(o);
+		assert.equal(Object.isFrozen(o), true);
+		assert.equal(Object.isFrozen(o.b), true);
+		assert.equal(Object.isFrozen(o.b.d), true);
+	});
+});
+
+function stub() {
+	return {
+		a: 1,
+		b: {
+			c: null,
+			d: [
+				1, 2, 3
+			]
+		}
+	};
+}
