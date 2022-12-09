90c992773c0d9ef1432498cbd452fc13e16ed99d
Nail Gafarov <nil20122013@gmail.com>
2022-12-09 09:33:56 +0500

    Added "object" module

---
+++ src/object.ts
@@ -0,0 +1,23 @@
+/**
+ * Same as {@link Object.seal} but performs the sealing recursively.
+ * @param object Object to seal.
+ */
+export function deepSeal(object: any): void {
+	applyRecursive(Object.seal, object);
+}
+
+/**
+ * Same as {@link Object.freeze} but performs the freezing recursively.
+ * @param object Object to
+ */
+export function deepFreeze(object: any): void {
+	applyRecursive(Object.freeze, object);
+}
+
+function applyRecursive(f: (o: any) => void, object: any): void {
+	if (object == null || typeof object !== "object")
+		return;
+	for (const key in object)
+		applyRecursive(f, object[key]);
+	f(object);
+}
---
+++ test/object.ts
@@ -0,0 +1,9 @@
+import * as assert from "assert";
+import * as mocha from "mocha";
+import * as object from "../src/object";
+
+// TODO
+mocha.describe("deepSeal()", () => {});
+
+// TODO
+mocha.describe("deepFreeze()", () => {});
