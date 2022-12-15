3384f3e2df5608cf8d7495cb3afffb563a4ae78f
Nail Gafarov <nil20122013@gmail.com>
2022-12-15 13:06:03 +0500

    Added time.ms() function stub

---
+++ src/time.ts
@@ -0,0 +1,14 @@
+/**
+ * Parses the given string into milliseconds.
+ * @param time String to parse.
+ * @returns Time in milliseconds or -1 if the string cannot be parsed.
+ * @example
+ * ```ts
+ * ms("1w 10mins"); // 605400000
+ * ms(""); // -1
+ * ```
+ */
+export function ms(time: string): number {
+	time;
+	return -1;
+}
---
+++ test/time.ts
@@ -0,0 +1,6 @@
+import * as assert from "assert";
+import * as mocha from "mocha";
+import * as time from "../src/time";
+
+// TODO
+mocha.describe("ms()", () => {});
