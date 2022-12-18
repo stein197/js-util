9b4d3ef8256c7b81835a17b027ccfe9d73cfff2a
Nail Gafarov <nil20122013@gmail.com>
2022-12-18 19:44:27 +0500

    Implemented array.diff() and array.uniq() functions

--- src/array.ts
+++ src/array.ts
@@ -1,9 +1,30 @@
-// TODO
-export function diff<T>(a: T[], b: T[]): T[] {
-	return [];
+/**
+ * Returns a difference between arrays.
+ * @param array An array from which to subtract elements.
+ * @param subtrahend An array of elements to subtract from the first one.
+ * @returns Difference between arrays.
+ * @example
+ * ```ts
+ * diff(["a", "b", "c"], ["b"]); // ["a", "c"]
+ * ```
+ */
+export function diff<T>(array: T[], subtrahend: T[]): T[] {
+	const result: T[] = [];
+	for (const item of array)
+		if (subtrahend.indexOf(item) < 0)
+			result.push(item);
+	return result;
 }
 
-// TODO
-export function uniq<T>(a: T[]): T[] {
-	return [];
+/**
+ * Removes duplicates from the array.
+ * @param array An array which elements to filter out.
+ * @returns An array with elements that do not occur more than once.
+ * @example
+ * ```ts
+ * uniq(["a", "b", "a"]); // ["a", "b"]
+ * ```
+ */
+export function uniq<T>(array: T[]): T[] {
+	return array.filter((v, i, a) => a.indexOf(v) === i);
 }
