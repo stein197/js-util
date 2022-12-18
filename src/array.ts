d1cd322c42dabe6c127924f5a670fc771dc44384
Nail Gafarov <nil20122013@gmail.com>
2022-12-18 19:37:28 +0500

    Added array module stub

---
+++ src/array.ts
@@ -0,0 +1,9 @@
+// TODO
+export function diff<T>(a: T[], b: T[]): T[] {
+	return [];
+}
+
+// TODO
+export function uniq<T>(a: T[]): T[] {
+	return [];
+}
---
+++ test/array.ts
@@ -0,0 +1,9 @@
+import * as assert from "assert";
+import * as mocha from "mocha";
+import * as array from "../src/array";
+
+// TODO
+mocha.describe("diff()", () => {});
+
+// TODO
+mocha.describe("uniq()", () => {});
