156b2e2da82e2064579643bd5b828a827f66e482
Nail Gafarov <nil20122013@gmail.com>
2023-01-09 12:35:30 +0500

    Basic html.selector() implementation

--- src/html.ts
+++ src/html.ts
@@ -1,3 +1,10 @@
+import * as string from "./string";
+
+const DEFAULT_SELECTOR_OPTIONS: SelectorOptions = {
+	exclude: ["class", "id", "style"],
+	spaces: true
+};
+
 /**
  * Returns the highest z-index value among element's children. It also takes "position: static" into account.
  * @param window Global window object.
@@ -16,8 +23,54 @@ export function getHighestZIndex(window: Window, element: Element): number {
 	return z;
 }
 
-// TODO: Implementation, documentation, tests
-// TODO: In array of selectors, make nth-child to specify what child is exactly searched
-export function selector(element: Element): string {
-	return "";
+// TODO: tests
+/**
+ * Makes CSS selector of the specified element.
+ * @param element Elements of which selector to create.
+ * @param options Additional options.
+ * @returns CSS selector to the element.
+ * @example
+ * ```ts
+ * selector($0); // "html > body:nth-child(2) > div#element.class-name[attr=\"value\"]:nth-child(1)"
+ * ```
+ */
+export function selector(element: Element, options: Partial<SelectorOptions> = DEFAULT_SELECTOR_OPTIONS): string {
+	const opts = options === DEFAULT_SELECTOR_OPTIONS ? DEFAULT_SELECTOR_OPTIONS : {...DEFAULT_SELECTOR_OPTIONS, ...options};
+	const result: string[] = [];
+	let curElement = element;
+	while (curElement != null) {
+		const typeSelector = curElement.tagName.toLowerCase();
+		const idSelector = curElement.getAttribute("id") ? ("#" + curElement.getAttribute("id")) : "";
+		const classSelector = curElement.classList.length ? ("." + [...curElement.classList].join(".")) : "";
+		const attrSelector = curElement.getAttributeNames().filter(attr => !opts.exclude.includes(attr)).map(attr => {
+			const value = curElement.getAttribute(attr);
+			return value ? `[${attr}="${string.escape(value)}"]` : `[${attr}]`;
+		}).join("");
+		const nthSelector = curElement.parentElement == null ? "" : `:nth-child(${Array.from(curElement.parentElement.children).indexOf(curElement) + 1})`;
+		result.unshift(typeSelector + idSelector + classSelector + attrSelector + nthSelector);
+		// @ts-ignore
+		curElement = curElement.parentElement;
+	}
+	return result.join(opts.spaces ? " > " : ">");
+}
+
+type SelectorOptions = {
+
+	/**
+	 * Which attributes should not be present in selector chain. Use empty array to allow all.
+	 * @defaultValue `["class", "id", "style"]`
+	 */
+	exclude: string[];
+
+	/**
+	 * Whether to use space around right arrow or not.
+	 * @defaultValue `true`.
+	 * @example
+	 * ```ts
+	 * selector($0, {space: true});  // "html > body:nth-child(2) > div"
+	 * selector($0, {space: false}); // "html>body:nth-child(2)>div"
+	 * ```
+	 */
+	spaces: boolean;
+	// TODO: quotes, onlyID, nth, root
 }
