210798392afeb26b40e2e70e1200ecfc013efc0b
Nail Gafarov <nil20122013@gmail.com>
2023-01-19 13:38:52 +0500

    Updated mime.is()

--- src/mime.ts
+++ src/mime.ts
@@ -4,7 +4,7 @@ const REGEX_MIME = /^(\*|[a-z0-9_-]+)\/(\*|[a-z0-9_-]+)$/
 export function is(type: string, mime: string): boolean {
 	const typeMatch = match(type);
 	const mimeMatch = match(mime);
-	return typeMatch != null && mimeMatch != null && (typeMatch[0] === "*" || typeMatch[0] === mimeMatch[0] && (typeMatch[1] === "*" || typeMatch[1] === mimeMatch[1]));
+	return typeMatch != null && mimeMatch != null && (typeMatch[0] === "*" && typeMatch[1] === "*" || typeMatch[0] === mimeMatch[0] && (typeMatch[1] === "*" || typeMatch[1] === mimeMatch[1]));
 }
 
 function match(mime: string): [type: string, subtype: string] | null {
