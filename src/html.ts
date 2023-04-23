8e5fd6d38a79d05222f070156270dc17d4058f90
Nail Gafarov <nil20122013@gmail.com>
2023-04-23 14:49:49 +0500

    Added documentation to html.getTableRow(), html.getTableCol() and html.getTable() functions

--- src/html.ts
+++ src/html.ts
@@ -108,12 +108,82 @@ export function getInputValue(input: HTMLInputElement | HTMLSelectElement | HTML
 	return input.value;
 }
 
-// TODO
+/**
+ * Gets HTML table element (table, thead, tbody or tfoot) and returns an array that represents the contents of the
+ * selected table row. By default it will try to cast some elements and values to corresponding types, such as casting
+ * "10" to 10 and inputs according to {@link getInputValue} function. It's possible to override that behavior by
+ * providing custom function that takes three arguments - row index, column index and HTMLTableCellElement itself.
+ * The value function returns will be pasted in the final array.
+ * @param table Table to parse.
+ * @param index Row index.
+ * @param handler Custom table cell handler function.
+ * @returns An array that represents the selected row or `null` if there is no row with specified index.
+ * @example
+ * Default behavior
+ * ```tsx
+ * const t = (
+ * 	<table>
+ * 		<tbody>
+ * 			<tr>
+ * 				<td>String</td>
+ * 				<td>10</td>
+ * 				<td>
+ * 					<input type="number" value="20" />
+ *	 			</td>
+ * 			</tr>
+ * 		</tbody>
+ * 	</table>
+ * );
+ * getTableRow(t, 0); // [["String", 10, 20]]
+ * getTableRow(t, 1); // null
+ * ```
+ * @example
+ * Overriding the handler
+ * ```tsx
+ * // Get previously defined t table
+ * getTableRow(t, 0, (row, col, cell) => cell.textContent); // ["String", "10", ""]
+ * ```
+ */
 export function getTableRow<T extends any[] = any[]>(table: HTMLTableElement | HTMLTableSectionElement, index: number, handler: TableCellHandler = handleTableCell): T | null {
 	return getTable(table, handler)[index] as T ?? null;
 }
 
-// TODO
+/**
+ * Gets HTML table element (table, thead, tbody or tfoot) and returns an array that represents the contents of the
+ * selected table column. By default it will try to cast some elements and values to corresponding types, such as
+ * casting "10" to 10 and inputs according to {@link getInputValue} function. It's possible to override that behavior by
+ * providing custom function that takes three arguments - row index, column index and HTMLTableCellElement itself.
+ * The value function returns will be pasted in the final array.
+ * @param table Table to parse.
+ * @param index Column index.
+ * @param handler Custom table cell handler function.
+ * @returns An array that represents the selected column or `null` if there is no column with specified index.
+ * @example
+ * Default behavior
+ * ```tsx
+ * const t = (
+ * 	<table>
+ * 		<tbody>
+ * 			<tr>
+ * 				<td>String</td>
+ * 				<td>10</td>
+ * 				<td>
+ * 					<input type="number" value="20" />
+ *	 			</td>
+ * 			</tr>
+ * 		</tbody>
+ * 	</table>
+ * );
+ * getTableCol(t, 0); // ["String"]
+ * getTableCol(t, 3); // null
+ * ```
+ * @example
+ * Overriding the handler
+ * ```tsx
+ * // Get previously defined t table
+ * getTableCol(t, 0, (row, col, cell) => cell.textContent); // ["String"]
+ * ```
+ */
 export function getTableCol<T extends any[] = any[]>(table: HTMLTableElement | HTMLTableSectionElement, index: number, handler: TableCellHandler = handleTableCell): T | null {
 	const t = getTable(table, handler);
 	const result = new Array(t.length) as T;
@@ -122,7 +192,40 @@ export function getTableCol<T extends any[] = any[]>(table: HTMLTableElement | H
 	return result.length || result.every(item => item == null) ? null : result;
 }
 
-// TODO
+/**
+ * Gets HTML table element (table, thead, tbody or tfoot) and returns an array that represents the contents of the
+ * table. By default it will try to cast some elements and values to corresponding types, such as casting "10" to 10 and
+ * inputs according to {@link getInputValue} function. It's possible to override that behavior by providing custom
+ * function that takes three arguments - row index, column index and HTMLTableCellElement itself. The value function
+ * returns will be pasted in the final array.
+ * @param table Table to parse.
+ * @param handler Custom table cell handler function.
+ * @returns An array that represents table.
+ * @example
+ * Default behavior
+ * ```tsx
+ * const t = (
+ * 	<table>
+ * 		<tbody>
+ * 			<tr>
+ * 				<td>String</td>
+ * 				<td>10</td>
+ * 				<td>
+ * 					<input type="number" value="20" />
+ *	 			</td>
+ * 			</tr>
+ * 		</tbody>
+ * 	</table>
+ * );
+ * getTable(t); // [["String", 10, 20]]
+ * ```
+ * @example
+ * Overriding the handler
+ * ```tsx
+ * // Get previously defined t table
+ * getTable(t, (row, col, cell) => cell.textContent); // [["String", "10", ""]]
+ * ```
+ */
 export function getTable(table: HTMLTableElement | HTMLTableSectionElement, handler: TableCellHandler = handleTableCell): any[][] {
 	return ((is(table, "table") ? [
 		...(table.tHead ? [...table.tHead.children] : []),
